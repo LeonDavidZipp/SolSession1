ac132f9a8271e7aada7cfc8f43a2b9cdc808c7f8
Leon David Zipp <leondavidzipp@gmx.de>
2024-07-25 01:07:16 +0200

    renamed params in cli

--- session_1/src/flare.ts
+++ session_1/src/flare.ts
@@ -20,7 +20,7 @@ program
     .command('airdrop')
     .description('Request an airdrop')
     .requiredOption('-a, --amount <amount>', 'Amount of SOL to request')
-    .requiredOption('-r, --receiverPublicKey <receiverPublicKey>', 'Public key of the receiver')
+    .requiredOption('-r, --receiver <receiverPublicKey>', 'Public key of the receiver')
     .action((amount: string, receiverPublicKeyString: string) => {
         requestAirdrop(Number(amount), new PublicKey(receiverPublicKeyString));
     });
@@ -29,8 +29,8 @@ program
     .command('send')
     .description('Send SOL')
     .requiredOption('-a, --amount <amount>', 'Amount of SOL to send')
-    .requiredOption('-s, --senderPrivateKey <senderPrivateKey>', 'Private key of the sender')
-    .requiredOption('-r, --receiverPublicKey <receiverPublicKey>', 'Public key of the receiver')
+    .requiredOption('-s, --sender <senderPrivateKey>', 'Private key of the sender')
+    .requiredOption('-r, --receiver <receiverPublicKey>', 'Public key of the receiver')
     .action((options: { amount: string, senderPrivateKey: string, receiverPublicKey: string }) => {
         const { amount, senderPrivateKey, receiverPublicKey } = options;
         sendSol(Number(amount), privateKeyStringToKeypair(senderPrivateKey), new PublicKey(receiverPublicKey));
