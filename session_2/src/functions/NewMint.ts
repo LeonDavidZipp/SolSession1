8ed5fe5a1e17f4df2d8e49802b25ba2925c4d255
LeondavidZipp <leondavidzipp@gmx.de>
2024-08-05 08:35:02 +0000

    added ChangeOwnerOfATA function body

--- session_2/src/functions/NewMint.ts
+++ session_2/src/functions/NewMint.ts
@@ -24,6 +24,8 @@ import {
     getOrCreateAssociatedTokenAccount,
     mintTo,
     burn,
+    setAuthority,
+    AuthorityType,
 } from "@solana/spl-token";
 
 /**
@@ -93,6 +95,20 @@ export const TransferTokens = async (
     );
 };
 
+
+/**
+ * Burn tokens from an account
+ * @param connection     Connection to use
+ * @param payer          Payer of the transaction fees
+ * @param account        Account to burn tokens from
+ * @param mint           Mint for the account
+ * @param owner          Account owner
+ * @param amount         Amount to burn
+ * @param multiSigners   Signing accounts if `owner` is a multisig
+ * @param confirmOptions Options for confirming the transaction
+ * @param programId      SPL Token program account
+ * @return Signature of the confirmed transaction
+ */
 export const BurnTokens = (
     connection: Connection,
     owner: Signer | PublicKey,
@@ -115,4 +131,39 @@ export const BurnTokens = (
     );
 };
 
-export const ChangeOwnerOfATA = () => {};
+/**
+
+ * Assign a new authority to the account
+ * @param connection       Connection to use
+ * @param payer            Payer of the transaction fees
+ * @param account          Address of the account
+ * @param currentAuthority Current authority of the specified type
+ * @param authorityType    Type of authority to set
+ * @param newAuthority     New authority of the account
+ * @param multiSigners     Signing accounts if `currentAuthority` is a multisig
+ * @param confirmOptions   Options for confirming the transaction
+ * @param programId        SPL Token program account
+ * @return Signature of the confirmed transaction
+ */
+export const ChangeOwnerOfATA = (
+    connection: Connection,
+        payer: Signer,
+        account: PublicKey,
+        currentAuthority: Signer | PublicKey,
+        authorityType: AuthorityType,
+        newAuthority: PublicKey | null,
+        multiSigners: Signer[] = [],
+        confirmOptions?: ConfirmOptions,
+):Promise<TransactionSignature> => {
+    return setAuthority(
+        connection,
+        payer,
+        account,
+        currentAuthority,
+        authorityType,
+        newAuthority,
+        multiSigners,
+        confirmOptions,
+TOKEN_PROGRAM_ID
+    );
+};
